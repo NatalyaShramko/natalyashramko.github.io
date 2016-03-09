6ba39c9d74b66fca10dde3b3742cb1c450f300d6
Natalya <natalyashramko@gmail.com>
2016-03-09 14:23:26 +0200

    Fixed fitness.

---
+++ fitness/js/main.js
@@ -0,0 +1,16 @@
+$("#home").click(function() {
+    $("html, body").animate({ scrollTop: 10 }, "slow");
+});
+$("#blog").click(function() {
+    $("html, body").animate({ scrollTop: 1400 }, "slow");
+});
+$("#about").click(function() {
+    $("html, body").animate({ scrollTop: 4200 }, "slow");
+});
+
+$('.advice-section a').click(function() {
+    $(this).attr('target','_blank');
+});
+$('.article-section a').click(function() {
+    $(this).attr('target','_blank');
+});
